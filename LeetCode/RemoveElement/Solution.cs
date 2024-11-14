ab778890cb3f85c6a45d3bceea4189fee4d0837a
Artem Vasilyev <artemvasilyev@kept.ru>
2024-11-14 18:05:42 +0300

    27. Remove element

---
+++ LeetCode/RemoveElement/Solution.cs
@@ -0,0 +1,29 @@
+﻿// https://leetcode.com/problems/remove-element/description/
+namespace LeetCode.RemoveElement;
+
+public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        var k = 0;
+        var arr = new List<int>();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == val)
+            {
+                k++;
+            }
+            else
+            {
+                arr.Add(nums[i]);
+            }
+        }
+
+        Array.Copy(arr.ToArray(), nums, arr.Count);
+        for (int i = arr.Count; i < nums.Length; i++)
+        {
+            nums[i] = 0;
+        }
+        return k;
+    }
+}
---
+++ TestProject/RemoveElement/RemoveElementTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.RemoveElement;
+
+namespace TestProject.RemoveElement;
+
+[TestFixture]
+public class RemoveElementTests
+{
+    [Test]
+    public void Test1()
+    {
+        var sol = new Solution();
+        int[] nums = [3, 2, 2, 3];
+        var k = sol.RemoveElement(nums, 3);
+        Assert.That(k, Is.EqualTo(2));
+        Assert.That(nums, Is.EquivalentTo(new[] { 2, 2 }));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        var sol = new Solution();
+        int[] nums = [0, 1, 2, 2, 3, 0, 4, 2];
+        var k = sol.RemoveElement(nums, 2);
+    }
+}
